303985c3e8399bac71e34a21ca96ada8a35a120d
sherwoodfi <blackandwhite90@gmail.com>
2020-08-05 01:17:01 -0400

    updating readme file

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # CSVReportGenerator
-Query a database according to a schedule, and send a CSV file of the output data
+
+CSVReportGenerator (CRG) is a tool made to pull data from a database and deliver it to the end-user in a CSV file.
+
+To properly function, CRG requires...
+
+- access to a google or other SMTP client
+- database credentials
+
+## Functional Overview
+
+The program is designed to be run once a day, typically early to get Ops/marketing personel required data early in the day. In the past, I typically had it run at 8:30am to deliver reports as people were sitting at their desks.
+
+The general flow is:
+
+1. CRG pulls all SQL files stored in the _queries_ directory and loops over a list of the files
+2. CRG validates that the file has been scheduled and whether it should be run on the current day
+3. If the query should run, CRG...
+   opens a database connection
+   runs the query stored in the _.sql_ file
+   loads returned data into a temp _.csv_ file
+   creates an SMTP MIMEmultipart message and attaches the _.csv_
+   sends it to all recipients stored in the scheduled list
+
+## Set up
+
+1. Create a scheduler object in the **schedule_manager.py** file
+2. options are...
+   dow: day of week, [0-6]
+   dom: day of month, [1-31]
+   qrt: quarterly sending
+
+```python
+# python scheduler objects
+schedule_manager = {
+ "sample_query.sql": {
+     "frequency": "dow",
+     "day_validator" : [1,2,3,4,5,6],
+     "receiver" : ["alex@protoboard.io"]
+ },
+}
+```
+
+3. in the **queries** directory,
+
+```sql
+select * from schema.database limit 10;
+```
+
+4. Schedule the code to run daily using either CRON or a cloud service like AWS Lambda + Cloudwatch
--- emailer.py
+++ emailer.py
@@ -16,46 +16,56 @@ load_dotenv(find_dotenv())
 d = datetime.datetime.now()
 d = d.strftime("%Y-%m-%d")
 
+
 def email_sender(requirements, report):
-    username = env.get('USERNAME')
-    password = env.get('PASSWORD')
+    username = env.get("USERNAME")
+    password = env.get("PASSWORD")
     recipients = requirements["receiver"]
     body = """Hello, 
 
         Your scheduled report: "{0}" is attached.
-    """.format(report.replace('_', ' '))
+    """.format(
+        report.replace("_", " ")
+    )
 
     msg = MIMEMultipart()
-    msg['Subject'] = "Protoboard: {}, {}".format(report.replace('_', ' '), d)
-    msg['From'] = username
-    msg['To'] = ", ".join(recipients)
+    msg["Subject"] = "Report: {}, {}".format(report.replace("_", " "), d)
+    msg["From"] = username
+    msg["To"] = ", ".join(recipients)
 
-    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(body, "plain"))
 
-    filename = "{0}_{1}.csv".format(report,d)
+    filename = "{0}_{1}.csv".format(report, d)
 
-    part = MIMEBase('application', "octet-stream")
+    part = MIMEBase("application", "octet-stream")
     part.set_payload(open("/tmp/{}".format(filename), "rb").read())
     encoders.encode_base64(part)
 
-    part.add_header('Content-Disposition', 'attachment; filename={}_{}.csv'.format(report, d))
+    part.add_header(
+        "Content-Disposition", "attachment; filename={}_{}.csv".format(report, d)
+    )
 
     msg.attach(part)
 
-    try:  
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    try:
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
         server.ehlo()
         server.login(username, password)
-        server.sendmail(msg['From'], recipients, msg.as_string())
+        server.sendmail(msg["From"], recipients, msg.as_string())
         server.close()
 
-    except Exception as e:  
+    except Exception as e:
         print(e)
 
 
 def report_grabber(query):
-    conn_string = "dbname='{}' port='{}' user='{}' password='{}' host='{}'"\
-            .format(env.get('DB_NAME'), env.get('DB_PORT'), env.get('DB_USER'), env.get('DB_PW'), env.get('DB_HOST'))
+    conn_string = "dbname='{}' port='{}' user='{}' password='{}' host='{}'".format(
+        env.get("DB_NAME"),
+        env.get("DB_PORT"),
+        env.get("DB_USER"),
+        env.get("DB_PW"),
+        env.get("DB_HOST"),
+    )
 
     try:
         con = pg.connect(conn_string)
@@ -76,8 +86,9 @@ def report_grabber(query):
 
     return response, colnames
 
-def csv_writer(headers, data ,report="report_from"):
-    report = open("/tmp/{0}_{1}.csv".format(report, d), "w") ## need /tmp/ for lambda
+
+def csv_writer(headers, data, report="report_from"):
+    report = open("/tmp/{0}_{1}.csv".format(report, d), "w")  ## need /tmp/ for lambda
     report_writer = csv.writer(report, delimiter=",")
     report_writer.writerow(headers)
 
@@ -92,28 +103,40 @@ def csv_writer(headers, data ,report="report_from"):
 
 def sql_scripts():
     scripts = []
-    
+
     for file in listdir("/var/task/queries"):
         if file.endswith(".sql"):
             scripts.append(file)
 
-    return(scripts)
+    return scripts
+
 
 def date_validator(requirements):
-    dom_actual = int(datetime.datetime.today().strftime('%d'))
+    dom_actual = int(datetime.datetime.today().strftime("%d"))
     dow_actual = int(datetime.datetime.today().weekday())
-    qrt_actual = int(datetime.datetime.today().strftime('%m'))
+    qrt_actual = int(datetime.datetime.today().strftime("%m"))
 
     should_run = False
 
-    if requirements["frequency"] == "dow" and dow_actual in requirements["day_validator"]:
+    if (
+        requirements["frequency"] == "dow"
+        and dow_actual in requirements["day_validator"]
+    ):
         should_run = True
-    elif requirements["frequency"] == "dom" and dom_actual in requirements["day_validator"]:
+    elif (
+        requirements["frequency"] == "dom"
+        and dom_actual in requirements["day_validator"]
+    ):
         should_run = True
-    elif requirements["frequency"] == "qrt" and dom_actual in requirements["day_validator"] and qrt_actual in [1,4,7,10]: ##first day of new quarter
+    elif (
+        requirements["frequency"] == "qrt"
+        and dom_actual in requirements["day_validator"]
+        and qrt_actual in [1, 4, 7, 10]
+    ):  ##first day of new quarter
         should_run = True
 
     return should_run
+
 
 def execute_script(event, context):
     scripts = sql_scripts()
@@ -123,8 +146,12 @@ def execute_script(event, context):
             if date_validator(schedule_manager[script]):
                 sql = open("/var/task/queries/{}".format(script), "r").read()
                 response, colnames = report_grabber(sql)
-                csv_writer(headers = colnames, data= response, report = script.replace('.sql', ''))
-                email_sender(schedule_manager[script], report = script.replace('.sql', ''))
+                csv_writer(
+                    headers=colnames, data=response, report=script.replace(".sql", "")
+                )
+                email_sender(
+                    schedule_manager[script], report=script.replace(".sql", "")
+                )
             else:
                 print("report: {} should not run".format(script))
         else:
